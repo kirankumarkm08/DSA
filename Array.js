e7d988c5e39145c17fd64e6a862cd90c0fc61f3c
kirankumarkm08 <kirankumarkm1505@gmail.com>
2024-09-05 13:50:07 +0530

    pushed

--- Array.js
+++ Array.js
@@ -1,2 +1,49 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+arr.pop(); // remove the last element
+arr.push(0); // add the last element
+arr.unshift(11); //add in 1st element
+arr.shift(); // remove in 1st element
+arr.map((i) => console.log(i)); // return each element in a new  array  with mutate the original array
+const slice = arr.slice(0, 2);
+console.log("Sliced array:", slice); // returns only selected elements
+
+const arr1 = [9, 2, 5, 1, 10, 100, -1];
+function compare(a, b) {
+  return a - b; // sort in descending order
+}
+
+console.log("sortde array :" + arr1.sort(compare));
+
+console.log("");
+
+for (let i of arr) {
+<<<<<<< Tabnine <<<<<<<
+/**//+
+ * This function iterates over an array and logs each element to the console.//+
+ *//+
+ * @param {Array} arr - The array to iterate over.//+
+ * @returns {undefined} This function does not return a value.//+
+ *///+
+function logArrayElements(arr) {//+
+  for (let i of arr) {//+
+    console.log(i);//+
+  }//+
+}//+
+>>>>>>> Tabnine >>>>>>>
+  console.log(i);
+}
+
+console.log(arr.some((i) => i % 10));
+
+const months = ["Jan", "March", "April", "June"];
+months.splice(1, 0, "Feb");
+// Inserts at index 1
+console.log(months);
+
+arr.splice(4, 0, 100);
+
 console.log(arr);
+
+const reducer = arr.reduce((items, curr) => items + curr);
+console.log("Sum of array elements:", reducer);
